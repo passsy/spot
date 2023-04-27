6ba9c4343081a4ae80aa36da86b22159be2d6cbf
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2023-04-27 23:12:09 +0700

    add passing tests

--- test/util_test.dart
+++ test/util_test.dart
@@ -2,26 +2,42 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:spot/spot.dart';
 
 void main() {
-  test('getAllSubsets', () {
-    final original = [1, 2, 3, 4];
-    final subsets = getAllSubsets(original);
-    const expected = [
-      [1, 2, 3],
-      [1, 2, 4],
-      [1, 3, 4],
-      [2, 3, 4],
-      [1, 2],
-      [1, 3],
-      [2, 3],
-      [1, 4],
-      [2, 4],
-      [3, 4],
-      [1],
-      [2],
-      [3],
-      [4],
-      []
-    ];
-    expect(subsets, expected);
+  group('getAllSubsets', () {
+    test('populated list', () {
+      final original = [1, 2, 3, 4];
+      final subsets = getAllSubsets(original);
+      const expected = [
+        [1, 2, 3],
+        [1, 2, 4],
+        [1, 3, 4],
+        [2, 3, 4],
+        [1, 2],
+        [1, 3],
+        [2, 3],
+        [1, 4],
+        [2, 4],
+        [3, 4],
+        [1],
+        [2],
+        [3],
+        [4],
+        [],
+      ];
+      expect(subsets, expected);
+    });
+
+    test('one entry', () {
+      final original = [1];
+      final subsets = getAllSubsets(original);
+      const expected = [[]];
+      expect(subsets, expected);
+    });
+
+    test('empty', () {
+      final original = [];
+      final subsets = getAllSubsets(original);
+      const expected = [];
+      expect(subsets, expected);
+    });
   });
 }
