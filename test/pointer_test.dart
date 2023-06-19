cebfe9f2b02e6c0496e76fc9bacd2a7e5ec330a7
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2023-06-19 20:14:48 +0200

    Add tests for TouchEvent.tap()

---
+++ test/pointer_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:spot/spot.dart';
+
+void main() {
+  testWidgets('test pointer', (tester) async {
+    int i = 0;
+    void onPressed() => i++;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: ElevatedButton(
+              onPressed: onPressed,
+              child: null,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final button = find.byType(ElevatedButton);
+    expect(button, findsOneWidget);
+    final buttonRect = tester.getRect(button);
+
+    expect(i, 0);
+    TouchEvent.tap(buttonRect.center);
+    expect(i, 1);
+    TouchEvent.tap(buttonRect.center);
+    expect(i, 2);
+  });
+}
