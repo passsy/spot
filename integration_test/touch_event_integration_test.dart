6ff13dbfec08ce96fdfca84c8d2fb4a3fd52d52e
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2023-06-23 11:28:59 +0200

    Remove code just for documenting

--- integration_test/touch_event_integration_test.dart
+++ integration_test/touch_event_integration_test.dart
@@ -24,40 +24,6 @@ void main() {
 
     final button = spotSingle<MaterialApp>().spotSingle<ElevatedButton>();
 
-    /// Can be removed - copied to [_TouchEvent]
-
-    // final snapshot = button.snapshot();
-    // final element = snapshot.discoveredElement;
-    // final box = element?.renderObject as RenderBox?;
-    // final position = box!.localToGlobal(box.size.center(Offset.zero));
-    // final downEvent = PointerDownEvent(position: position);
-    // final upEvent = PointerUpEvent(position: position);
-    //
-    // binding.handlePointerEvent(downEvent);
-    // binding.handlePointerEvent(upEvent);
-    //
-    // binding.handlePointerEvent(downEvent);
-    // binding.handlePointerEvent(upEvent);
-    //
-    // expect(i, 2);
-    // i = 0;
-
-    /// Unnecessary, since dispatchEvent() doesn't work:
-    // final result = BoxHitTestResult();
-    // final entry = BoxHitTestEntry(box, position);
-    // result.add(entry);
-
-    /// Doesn't work, because it's calling [dispatchEvent] from abstract class [HitTestDispatcher]:
-    // binding.deviceEventDispatcher?.dispatchEvent(event, result);
-
-    /// Doesn't increment:
-    // dispatcher?.binding.dispatchEvent(downEvent, result);
-    // dispatcher?.binding.dispatchEvent(upEvent, result);
-
-    /// Doesn't increment:
-    // (dispatcher?.binding as LiveTestWidgetsFlutterBinding?)?.dispatchEvent(downEvent, result);
-    // (dispatcher?.binding as LiveTestWidgetsFlutterBinding?)?.dispatchEvent(upEvent, result);
-
     expect(i, 0);
     button.tap();
     expect(i, 1);
