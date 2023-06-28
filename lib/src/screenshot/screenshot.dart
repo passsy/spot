48106ef720b13a4b66185149306f47b93403e54f
Pascal Welsch <pascal@welsch.dev>
2023-06-28 23:39:09 +0200

    Allow setting a selector/snapshot/element to take a screenshot from

--- lib/src/screenshot/screenshot.dart
+++ lib/src/screenshot/screenshot.dart
@@ -8,38 +8,68 @@ import 'package:dartx/dartx_io.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:spot/spot.dart';
 import 'package:stack_trace/stack_trace.dart';
 import 'package:test_api/hooks.dart';
 import 'package:ulid/ulid.dart';
 
-Future<File> takeScreenshot({String? name, bool print = true}) async {
-  final frame = _caller();
-  final callerFileName = () {
-    final file = frame?.uri.pathSegments.last
-        .replaceFirst('_test.dart', '')
-        .replaceFirst('.dart', '');
-    final line = frame?.line;
-    if (file != null && line != null) {
-      return '$file:$line';
-    }
-    if (file != null) {
-      return file;
-    }
-    return 'unknown';
-  }();
+class Screenshot {
+  Screenshot({required this.file});
+  final File file;
+}
 
-  final String screenshotFileName = () {
-    final n = name ?? callerFileName;
-    final uniqueId = Ulid().toCanonical().substring(0, 5); // nanoid(5)
-    return '$n-$uniqueId.png';
-  }();
-
+Future<Screenshot> takeScreenshot({
+  Element? element,
+  SingleWidgetSnapshot? snapshot,
+  SingleWidgetSelector? selector,
+  String? name,
+  bool print = true,
+}) async {
   final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.instance;
+  final frame = _caller();
+
+  // Element that is currently active in the widget tree, to take a screenshot of
+  final Element liveElement = () {
+    if (selector != null) {
+      // taking a fresh snapshot guarantees an element that is currently in the
+      // tree and can be screenshotted
+      final snapshot = selector.snapshot().existsOnce();
+      return snapshot.element;
+    }
+
+    if (snapshot != null) {
+      if (!snapshot.element.mounted) {
+        throw StateError(
+          'Can not take a screenshot of snapshot $snapshot, because it is not mounted anymore. '
+          'Only Elements that are currently mounted can be screenshotted.',
+        );
+      }
+      if (snapshot.widget != snapshot.element.widget) {
+        throw StateError(
+          'Can not take a screenshot of snapshot $snapshot, because the Element has been updated since the snapshot was taken. '
+          'This happens when the widget tree is rebuilt.',
+        );
+      }
+      return snapshot.element;
+    }
+
+    if (element != null) {
+      if (!element.mounted) {
+        throw StateError(
+          'Can not take a screenshot of Element $element, because it is not mounted anymore. '
+          'Only Elements that are currently mounted can be screenshotted.',
+        );
+      }
+      return element;
+    }
+
+    // fallback to screenshotting the entire app
+    return binding.renderViewElement!;
+  }();
 
   late final Uint8List bytes;
   await binding.runAsync(() async {
-    final element = binding.renderViewElement!;
-    final image = await _captureImage(element);
+    final image = await _captureImage(liveElement);
     final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
     if (byteData == null) {
       return 'Could not take screenshot';
@@ -52,6 +82,26 @@ Future<File> takeScreenshot({String? name, bool print = true}) async {
   if (!spotTempDir.existsSync()) {
     spotTempDir.createSync();
   }
+  String callerFileName() {
+    final file = frame?.uri.pathSegments.last
+        .replaceFirst('_test.dart', '')
+        .replaceFirst('.dart', '');
+    final line = frame?.line;
+    if (file != null && line != null) {
+      return '$file:$line';
+    }
+    if (file != null) {
+      return file;
+    }
+    return 'unknown';
+  }
+
+  final String screenshotFileName = () {
+    final n = name ?? callerFileName();
+    // always append a unique id to avoid name collisions
+    final uniqueId = Ulid().toCanonical().substring(0, 5); // nanoid(5)
+    return '$n-$uniqueId.png';
+  }();
   final file = spotTempDir.file(screenshotFileName);
   file.writeAsBytesSync(bytes);
   if (print) {
@@ -61,7 +111,7 @@ Future<File> takeScreenshot({String? name, bool print = true}) async {
       '  taken at ${frame?.member} ${frame?.uri}:${frame?.line}:${frame?.column}',
     );
   }
-  return file;
+  return Screenshot(file: file);
 }
 
 /// Render the closest [RepaintBoundary] of the [element] into an image.
