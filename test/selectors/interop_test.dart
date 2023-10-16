b6db6457659f94e2adc9a7f45f1f010b3abe4ccd
Pascal Welsch <pascal@welsch.dev>
2023-10-16 10:58:58 +0200

    Fix formatting

--- test/selectors/interop_test.dart
+++ test/selectors/interop_test.dart
@@ -53,11 +53,13 @@ void main() {
             anyOf(
               // Flutter 3.13
               contains(
-                  'Could not find widget with text "nope" which is an ancestor of type "Directionality" in widget tree'),
+                'Could not find widget with text "nope" which is an ancestor of type "Directionality" in widget tree',
+              ),
 
               // Flutter 3.15
               contains(
-                  'Could not find widget with widgets with text "nope" that are ancestors of widgets with type "Directionality" in widget tree'),
+                'Could not find widget with widgets with text "nope" that are ancestors of widgets with type "Directionality" in widget tree',
+              ),
             ),
           ),
         ),
