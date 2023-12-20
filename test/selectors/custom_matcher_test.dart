1db177ce888407de154888ecb31f35e02d2dc16d
Pascal Welsch <pascal@welsch.dev>
2023-12-20 13:33:54 +0100

    Fix imports

--- test/selectors/custom_matcher_test.dart
+++ test/selectors/custom_matcher_test.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spot/spot.dart';
 import 'package:spot/src/spot/selectors.dart';
-import 'package:spot/src/spot/snapshot.dart';
 
 void main() {
   group('match custom property', () {
