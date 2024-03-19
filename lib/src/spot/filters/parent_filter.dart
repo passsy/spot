816fec6b85de8e14aa6cabc7218fac2f287e56a0
Maikel Rehl <maikel@phntm.xyz>
2024-03-19 15:52:26 +0100

    Remove import

--- lib/src/spot/filters/parent_filter.dart
+++ lib/src/spot/filters/parent_filter.dart
@@ -2,7 +2,6 @@ import 'package:dartx/dartx.dart';
 import 'package:flutter/widgets.dart';
 import 'package:spot/spot.dart';
 import 'package:spot/src/spot/snapshot.dart';
-import 'package:spot/src/spot/tree_snapshot.dart';
 import 'package:spot/src/spot/widget_selector.dart';
 
 /// A filter that checks if the candidates are children of all [parents]
