0ea2f7a637a06a5e35996d8f01cb771a968729b1
danielmolnar <daniel-molnar@web.de>
2024-05-22 17:45:44 +0200

    Add test "Cease recording live timeline after first tap"

--- test/timeline/timeline_test.dart
+++ test/timeline/timeline_test.dart
@@ -29,6 +29,32 @@ void main() {
         await percentageOfPixelsWithColor(shot.file, const Color(0xFF00FFFF));
     expect(cyanPixelCoverage, greaterThan(0.0));
   });
+
+  testWidgets('Cease recording live timeline after first tap', (tester) async {
+    final Timeline timeline = liveTimeline();
+    tester.view.physicalSize = const Size(210, 210);
+    tester.view.devicePixelRatio = 1.0;
+    int i = 0;
+    await tester.pumpWidget(
+      _TimelineTestWidget(
+        onTap: () {
+          i++;
+        },
+      ),
+    );
+
+    final button = spotIcon(Icons.home)..existsOnce();
+    await act.tap(button);
+    expect(i, 1);
+    expect(timeline.events.length, 1);
+    await act.tap(button);
+    expect(i, 2);
+    expect(timeline.events.length, 2);
+    timeline.stopTimeLine();
+    await act.tap(button);
+    expect(i, 3);
+    expect(timeline.events.length, 2);
+  });
 }
 
 class _TimelineTestWidget extends StatelessWidget {
