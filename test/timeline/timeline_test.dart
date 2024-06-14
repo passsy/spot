8f711726e118dd53f38679135e21f244431a599e
danielmolnar <daniel-molnar@web.de>
2024-06-14 17:23:58 +0200

    Add test "Live timeline - with error, no duplicates, prints HTML"

--- test/timeline/timeline_test.dart
+++ test/timeline/timeline_test.dart
@@ -311,6 +311,78 @@ void main() {
         isTrue,
       );
     });
+    test('Live timeline - with error, no duplicates, prints HTML', () async {
+      final tempDir = Directory.systemTemp.createTempSync();
+      final tempTestFile = File('${tempDir.path}/temp_test.dart');
+      await tempTestFile.writeAsString(
+        _testAsString(
+          title: 'Live timeline - with error, no duplicates, prints HTML',
+          timelineMode: TimelineMode.live,
+          shouldFail: true,
+        ),
+      );
+
+      final testProcess =
+          await TestProcess.start('flutter', ['test', tempTestFile.path]);
+
+      final stdoutBuffer = StringBuffer();
+
+      bool write = false;
+      await for (final line in testProcess.stdoutStream()) {
+        if (line.isEmpty) continue;
+
+        if (!write) {
+          if (line == _header) {
+            write = true;
+          }
+        }
+
+        if (write) {
+          stdoutBuffer.writeln(line);
+        }
+      }
+
+      // Error does not happen
+      await testProcess.shouldExit(1);
+      tempDir.deleteSync(recursive: true);
+
+      final stdout = stdoutBuffer.toString();
+      final timeline = stdout.split('\n');
+      // Does not start with 'Timeline', this only happens on error
+      expect(timeline.first, _header);
+      expect(
+        timeline.second,
+        'Event: Tap Icon Widget with icon: "IconData(U+0E047)"',
+      );
+      expect(
+        timeline[2].startsWith('Caller: at main.<fn> file:///'),
+        isTrue,
+      );
+      expect(
+        timeline[3].startsWith(
+          'Screenshot: file:///',
+        ),
+        isTrue,
+      );
+      expect(
+        timeline[4].startsWith(
+          'Timestamp:',
+        ),
+        isTrue,
+      );
+      expect(
+        timeline[5],
+        _separator,
+      );
+      final htmlLine = timeline
+          .firstWhere((line) => line.startsWith('View time line here:'));
+      expect(
+        htmlLine.endsWith(
+          'live_timeline_-_with_error,_no_duplicates,_prints_html.html',
+        ),
+        isTrue,
+      );
+    });
   });
 }
 
