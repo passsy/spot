c9e6c72a7c01ef0ab46f567887b81b9da83358ad
robiness <robin.schonau@gmail.com>
2024-11-12 13:32:40 +0100

    More complex test

--- test/util/load_app_fonts/app_font_test.dart
+++ test/util/load_app_fonts/app_font_test.dart
@@ -1,7 +1,46 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
+import 'utils.dart';
+
 void main() {
+  print('flutterPath:');
   test('When defined in pubspec a third-party font is loaded', () async {
-    expect(true, true);
+    int exitCode = -1;
+    late Directory tempDir;
+    print('flutterPath: ');
+    print('flutterPath: $flutterPath');
+    try {
+      print('this should work right?');
+      // // Copy virtual/montserrat to test folder
+      // tempDir = Directory.systemTemp.createTempSync('app_font_test');
+      // final testProjectDir = tempDir.path;
+      //
+      // Directory(
+      //   '${Directory.current.path}/test/util/load_app_fonts/virtual/app_font',
+      // ).copyTo(testProjectDir);
+      //
+      // // Run pub get
+      // await Process.run(
+      //   flutterPath,
+      //   ['pub', 'get'],
+      //   workingDirectory: testProjectDir,
+      // );
+      //
+      // // Run tests
+      // final test = await Process.start(
+      //   flutterPath,
+      //   ['test'],
+      //   workingDirectory: testProjectDir,
+      // );
+      //
+      // exitCode = await test.exitCode;
+    } catch (e) {
+      rethrow;
+    } finally {
+      expect(true, true);
+      // tempDir.deleteSync(recursive: true);
+    }
   });
 }
