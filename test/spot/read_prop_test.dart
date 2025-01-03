71e332e863bc4426b88ba622ae3fc9f95c266573
Pascal Welsch <pascal@welsch.dev>
2025-01-03 17:49:08 +0100

    Remove unused import

--- test/spot/read_prop_test.dart
+++ test/spot/read_prop_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spot/spot.dart';
-import 'package:spot/src/spot/props.dart';
 
 void main() {
   group('WidgetSelector', () {
