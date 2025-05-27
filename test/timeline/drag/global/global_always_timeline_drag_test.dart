51d3b6a89b8199f8c47ec8b512c3f0b76e6e2331
Pascal Welsch <pascal@phntm.xyz>
2025-05-27 17:07:07 +0200

    Change position of parameter for older Dart versions

--- test/timeline/drag/global/global_always_timeline_drag_test.dart
+++ test/timeline/drag/global/global_always_timeline_drag_test.dart
@@ -6,10 +6,14 @@ import '../act_drag_timeline_test_bodies.dart';
 
 void main() {
   globalTimelineMode = TimelineMode.always;
-  group('Global: always', skip: kIsWeb, retry: 2, () {
-    testWidgets('turn off during test', (tester) async {
-      await ActDragTimelineTestBodies.recordTurnOff(tester);
-    });
+  group('Global: always', skip: kIsWeb, () {
+    testWidgets(
+      'turn off during test',
+      (tester) async {
+        await ActDragTimelineTestBodies.recordTurnOff(tester);
+      },
+      retry: 2,
+    );
     testWidgets('no error print nothing', (tester) async {
       await ActDragTimelineTestBodies.alwaysNoError(tester);
     });
